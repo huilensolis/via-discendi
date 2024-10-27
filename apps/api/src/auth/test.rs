7850c95365f02e754fbd3f87aa125d0382c823cd
Ikramullah <104609018+betester@users.noreply.github.com>
2024-10-28 01:35:54 +0700

    [API] Fix test due to race condition (#14)

--- apps/api/src/auth/test.rs
+++ apps/api/src/auth/test.rs
@@ -1,25 +1,30 @@
 #[cfg(test)]
 mod tests {
 
-    use core::panic;
-    use std::cmp::Ordering;
     use chrono::{Duration, Local};
+    use core::panic;
     use sqlx::{migrate::Migrator, postgres::PgPoolOptions};
+    use std::cmp::Ordering;
 
-
-    use crate::auth::{add_user, create_user_session, find_user, login, sign_up, User, DEFAULT_SESSION_DURATION_MIN};
+    use crate::auth::{
+        add_user, create_user_session, find_user, login, sign_up, User,
+        DEFAULT_SESSION_DURATION_MIN,
+    };
 
     #[tokio::test]
     async fn test_add_user() {
-
         let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect("postgres://myuser:mypassword@localhost/mydatabase").await.unwrap();
+            .max_connections(5)
+            .connect("postgres://myuser:mypassword@localhost/mydatabase")
+            .await
+            .unwrap();
 
-        let m = Migrator::new(std::path::Path::new("./migrations")).await.unwrap();
+        let m = Migrator::new(std::path::Path::new("./migrations"))
+            .await
+            .unwrap();
         m.run(&pool).await.unwrap();
-        
-        let mock_user = User{
+
+        let mock_user = User {
             username: String::from("test"),
             email: String::from("test@gmail.com"),
             password: String::from("unhashed"),
@@ -31,26 +36,28 @@ mod tests {
         let result = add_user(&mock_user, &pool).await;
         match result {
             Ok(value) => {
-                assert!(value.rows_affected() ==  1); 
-            },
+                assert!(value.rows_affected() == 1);
+            }
             Err(err) => {
                 panic!("Failed inserting mock user {}", err);
-            },
+            }
         }
-
-        m.undo(&pool, 1).await.unwrap();
     }
 
     #[tokio::test]
     async fn test_find_user() {
         let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect("postgres://myuser:mypassword@localhost/mydatabase").await.unwrap();
+            .max_connections(5)
+            .connect("postgres://myuser:mypassword@localhost/mydatabase")
+            .await
+            .unwrap();
 
-        let m = Migrator::new(std::path::Path::new("./migrations")).await.unwrap();
+        let m = Migrator::new(std::path::Path::new("./migrations"))
+            .await
+            .unwrap();
         m.run(&pool).await.unwrap();
-        
-        let mock_user = User{
+
+        let mock_user = User {
             username: String::from("test2"),
             email: String::from("test2@gmail.com"),
             password: String::from("unhashed"),
@@ -66,25 +73,30 @@ mod tests {
         match result {
             Ok(value) => {
                 assert!(value.eq(&mock_user))
-            },
+            }
             Err(err) => {
-                panic!("Failed finding user with username {} with  root cause {}", mock_user.username, err);
-            },
+                panic!(
+                    "Failed finding user with username {} with  root cause {}",
+                    mock_user.username, err
+                );
+            }
         }
-
-        m.undo(&pool, 1).await.unwrap();
     }
 
     #[tokio::test]
     async fn test_sign_up() {
         let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect("postgres://myuser:mypassword@localhost/mydatabase").await.unwrap();
+            .max_connections(5)
+            .connect("postgres://myuser:mypassword@localhost/mydatabase")
+            .await
+            .unwrap();
 
-        let m = Migrator::new(std::path::Path::new("./migrations")).await.unwrap();
+        let m = Migrator::new(std::path::Path::new("./migrations"))
+            .await
+            .unwrap();
         m.run(&pool).await.unwrap();
 
-        let mut mock_user = User{
+        let mut mock_user = User {
             username: String::from("test3"),
             email: String::from("test3@gmail.com"),
             password: String::from("unhashed"),
@@ -100,25 +112,27 @@ mod tests {
                 if !value {
                     panic!("Should not return false if no error occured")
                 }
-            },
+            }
             Err(err) => {
                 panic!("Failed signing up account with cause {}", err)
-            },
+            }
         }
-
-        m.undo(&pool, 1).await.unwrap();
     }
 
     #[tokio::test]
     async fn test_login() {
         let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect("postgres://myuser:mypassword@localhost/mydatabase").await.unwrap();
+            .max_connections(5)
+            .connect("postgres://myuser:mypassword@localhost/mydatabase")
+            .await
+            .unwrap();
 
-        let m = Migrator::new(std::path::Path::new("./migrations")).await.unwrap();
+        let m = Migrator::new(std::path::Path::new("./migrations"))
+            .await
+            .unwrap();
         m.run(&pool).await.unwrap();
 
-        let mut mock_user = User{
+        let mut mock_user = User {
             username: String::from("test5"),
             email: String::from("test5@gmail.com"),
             password: String::from("unhashed"),
@@ -129,26 +143,28 @@ mod tests {
 
         sign_up(&mut mock_user, &pool).await.unwrap();
 
-        let result = login(&String::from("test3"), &String::from("unhashed"), &pool).await;
+        let result = login(&String::from("test5"), &String::from("unhashed"), &pool).await;
 
         match result {
-            Ok(_) => {},
+            Ok(_) => {}
             Err(err) => panic!("{}", err),
         }
-
-        m.undo(&pool, 1).await.unwrap();
     }
 
-    #[tokio::test] 
+    #[tokio::test]
     async fn test_create_user_session() {
         let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect("postgres://myuser:mypassword@localhost/mydatabase").await.unwrap();
+            .max_connections(5)
+            .connect("postgres://myuser:mypassword@localhost/mydatabase")
+            .await
+            .unwrap();
 
-        let m = Migrator::new(std::path::Path::new("./migrations")).await.unwrap();
+        let m = Migrator::new(std::path::Path::new("./migrations"))
+            .await
+            .unwrap();
         m.run(&pool).await.unwrap();
 
-        let mut mock_user = User{
+        let mut mock_user = User {
             username: String::from("test4"),
             email: String::from("test4@gmail.com"),
             password: String::from("unhashed"),
@@ -164,13 +180,17 @@ mod tests {
         match result {
             Ok(created_session) => {
                 // minus one minutes because some milliseconds delay making sure it always be in range 30 minutes
-                let current_time = Local::now() + Duration::minutes(DEFAULT_SESSION_DURATION_MIN) - Duration::minutes(1);
-                assert!(created_session.expiry_date.unwrap().cmp(&current_time.naive_utc()) == Ordering::Greater)
-            },
+                let current_time = Local::now() + Duration::minutes(DEFAULT_SESSION_DURATION_MIN)
+                    - Duration::minutes(1);
+                assert!(
+                    created_session
+                        .expiry_date
+                        .unwrap()
+                        .cmp(&current_time.naive_utc())
+                        == Ordering::Greater
+                )
+            }
             Err(err) => panic!("{}", err.to_string()),
         }
-
-        m.undo(&pool, 1).await.unwrap();
     }
-
 }
