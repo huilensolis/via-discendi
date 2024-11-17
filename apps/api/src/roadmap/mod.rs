27f65686bfa0a6d594facb33d04ab2620850f5e4
Ikram <ikramzulkarnaen02@gmail.com>
2024-11-17 20:37:01 +0700

    add things to do

--- apps/api/src/roadmap/mod.rs
+++ apps/api/src/roadmap/mod.rs
@@ -1,14 +1,103 @@
 use chrono::NaiveDateTime;
+use sqlx;
+use sqlx::postgres::PgQueryResult;
+use sqlx::PgPool;
 
 pub mod api;
 
 #[derive(Debug)]
-pub struct Roadmap {
+pub struct Roadmaps {
+    // NOTE: both id and version makes the unique identifier for the roadmap
+    id: String,
+    version: String,
     tile: String,
     description: String,
-    like_count: u32,
-    author_id: string,
+    author_id: String,
+    published: bool, //NOTE: indicates whether it can be seen by other user
+    created_at: Option<NaiveDateTime>,
+    updated_at: Option<NaiveDateTime>,
+}
+
+pub struct UserRoadmaps {
+    user_id: String,
+    roadmap_id: String,
+    completed_areas: Vec<String>, //NOTE: store id of areas
+    created_at: Option<NaiveDateTime>,
+    updated_at: Option<NaiveDateTime>,
+}
+
+#[derive(Debug)]
+pub struct RoadmapLikes {
+    user_id: String,
+    roadmap_id: String,
+}
+
+#[derive(Debug)]
+pub struct RoadmapBookmarkers {
+    user_id: String,
+    roadmap_id: String,
+}
+
+#[derive(Debug)]
+pub struct Areas {
+    id: String,
+    parent_id: String,
+    roadmap_id: String,
+    title: String,
+    description: String,
+    things_to_learn: Vec<String>,
 
     created_at: Option<NaiveDateTime>,
     updated_at: Option<NaiveDateTime>,
+}
+
+async fn add_roadmap(roadmap: &Roadmaps, pool: &PgPool) -> Result<PgQueryResult, sqlx::Error> {
+    todo!()
+}
+
+async fn update_roadmap(roadmap: &Roadmaps, pool: &PgPool) -> Result<PgQueryResult, sqlx::Error> {
+    todo!()
+}
+
+async fn delete_roadmap(roadmap_id: String, pool: &PgPool) -> Result<PgQueryResult, sqlx::Error> {
+    todo!()
+}
+
+async fn find_roadmap(roadmap_name: String, pool: &PgPool) -> Result<PgQueryResult, sqlx::Error> {
+    todo!()
+}
+
+async fn like_roadmap(roadmap_id: String, pool: &PgPool) -> Result<PgQueryResult, sqlx::Error> {
+    todo!()
+}
+
+async fn get_user_bookmarked_roadmaps(
+    user_id: String,
+    pool: &PgPool,
+) -> Result<PgQueryResult, sqlx::Error> {
+    todo!()
+}
+
+async fn get_user_roadmaps(user_id: String, pool: &PgPool) -> Result<PgQueryResult, sqlx::Error> {
+    todo!();
+}
+
+async fn get_roadmaps(
+    offset: u32,
+    limit: u32,
+    pool: &PgPool,
+) -> Result<PgQueryResult, sqlx::Error> {
+    todo!();
+}
+
+async fn add_areas(area: &Areas, pool: &PgPool) -> Result<PgQueryResult, sqlx::Error> {
+    todo!()
+}
+
+async fn update_areas(area: &Areas, pool: &PgPool) -> Result<PgQueryResult, sqlx::Error> {
+    todo!()
+}
+
+async fn delete_area(area_id: String, pool: &PgPool) -> Result<PgQueryResult, sqlx::Error> {
+    todo!()
 }
